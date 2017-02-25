b0ae01a72d924410e594ea100a2eb4a23c497c18
Josue Breton Mini <josuebre@gmail.com>
2017-02-24 16:08:51 -0800

    Primer Commit real de Doit

--- Doit/ViewController.swift
+++ Doit/ViewController.swift
@@ -3,7 +3,7 @@
 //  Doit
 //
 //  Created by Josue Breton Mini on 2/24/17.
-//  Copyright © 2017 Josue Breton Mini. All rights reserved.
+//  Copyright © 2017 Josue Breton Mini. All rights reserved...
 //
 
 import UIKit
@@ -15,9 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+ 
     }
 
 
