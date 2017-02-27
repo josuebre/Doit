0598cdce22281e1e3197df1ff8c7fff497ce059c
Josue Breton Mini <josuebre@gmail.com>
2017-02-27 11:11:23 -0800

    me quede a medias, pero salamonda, se fue en putiza este guay…

---
+++ Doit/completeTaskViewController.swift
@@ -0,0 +1,29 @@
+//
+//  completeTaskViewController.swift
+//  Doit
+//
+//  Created by Josue Breton Mini on 2/25/17.
+//  Copyright © 2017 Josue Breton Mini. All rights reserved.
+//
+
+import UIKit
+
+class completeTaskViewController: UIViewController {
+
+    
+    
+    var task = Task()
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+
+}
